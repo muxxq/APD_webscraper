cf68d0b07c7dd8e36ed359a06a0fcff50bb7001b
muxxq <maximilianandrei2101@gmail.com>
2026-03-17 22:09:09 +0200

    Refactor CSV writing to include execution time and improve code structure

--- sequential/seq_scraper.py
+++ sequential/seq_scraper.py
@@ -1,30 +1,39 @@
+from ast import main
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-url = "https://books.toscrape.com/catalogue/page-1.html"
+def fetch_page(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
 
-start_time = time.time()
+def parse_page(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    books = soup.find_all("article", class_="product_pod")
+    return books
 
-response = requests.get(url)
-print('status', response.status_code)
-soup = BeautifulSoup(response.text, 'html.parser')
+def write_to_csv(books,start_time):
+    with open('data/output/books.csv', 'w', encoding='utf-8') as csvfile:
+        csvfile.write("Title,Price,Execution Time\n")
+        for book in books:
+            title = book.h3.a['title']
+            price = book.find("p", class_="price_color").text.strip()
+            csvfile.write(f"{title},{price}\n")
+        end_time = time.time() - start_time
+        csvfile.write(f"Execution Time: {end_time:.2f} seconds\n")
+def main():
 
-books = soup.find_all("article", class_="product_pod")
-print('found books', len(books))
+    url = "https://books.toscrape.com/catalogue/page-1.html"
 
-for book in books:
-    title = book.h3.a['title']
-    price = book.find("p", class_="price_color").text.strip()
-    print(f"Title: {title}, Price: {price}")
+    start_time = time.time()
 
-with open('data/output/books.csv', 'w', encoding='utf-8') as f:
-    f.write("Title,Price\n")
-    for book in books:
-        title = book.h3.a['title']
-        price = book.find("p", class_="price_color").text.strip()
-        f.write(f"{title},{price}\n")
+    html = fetch_page(url)
+    books = parse_page(html)
 
-end_time = time.time()
-print(f"Execution time: {end_time - start_time:.2f} seconds")
+    write_to_csv(books,start_time)
+
+if __name__ == "__main__":
+    main()
