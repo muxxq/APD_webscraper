b33aa3fbbb5cd6f55668d9f922e940a61bc91460
muxxq <maximilianandrei2101@gmail.com>
2026-03-17 20:23:45 +0200

    Add execution time measurement and output CSV file for scraped book data

--- sequential/seq_scraper.py
+++ sequential/seq_scraper.py
@@ -5,6 +5,8 @@ import time
 
 url = "https://books.toscrape.com/catalogue/page-1.html"
 
+start_time = time.time()
+
 response = requests.get(url)
 print('status', response.status_code)
 soup = BeautifulSoup(response.text, 'html.parser')
@@ -16,3 +18,13 @@ for book in books:
     title = book.h3.a['title']
     price = book.find("p", class_="price_color").text.strip()
     print(f"Title: {title}, Price: {price}")
+
+with open('data/output/books.csv', 'w', encoding='utf-8') as f:
+    f.write("Title,Price\n")
+    for book in books:
+        title = book.h3.a['title']
+        price = book.find("p", class_="price_color").text.strip()
+        f.write(f"{title},{price}\n")
+
+end_time = time.time()
+print(f"Execution time: {end_time - start_time:.2f} seconds")
